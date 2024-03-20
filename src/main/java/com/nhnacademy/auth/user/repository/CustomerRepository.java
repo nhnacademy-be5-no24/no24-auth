5975e157fffd5bc0c572c33866dd64b71ea3a28d
KIM BYEONG JU <100591433+hagisilm@users.noreply.github.com>
2024-03-20 17:15:57 +0900

    feat: jpa,mysql 의존성추가 및 mysql환경설정

---
+++ src/main/java/com/nhnacademy/auth/user/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.nhnacademy.auth.user.repository;
+
+import com.nhnacademy.auth.user.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+}
