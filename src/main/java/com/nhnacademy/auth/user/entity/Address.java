2987102d4807bd0e9353d4963932447f665749d1
KIM BYEONG JU <100591433+hagisilm@users.noreply.github.com>
2024-03-21 21:41:33 +0900

    chore: 기존파일 삭제 및 수정

--- src/main/java/com/nhnacademy/auth/user/entity/Address.java
+++ src/main/java/com/nhnacademy/auth/user/entity/Address.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -14,6 +12,7 @@ import javax.persistence.Id;
 @AllArgsConstructor
 public class Address {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "address_id")
     private Long addressId;
     @Column(name = "address_name")
