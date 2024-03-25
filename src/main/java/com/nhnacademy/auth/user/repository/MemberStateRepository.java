480d4388463120eb22842cd0e26e762a1afb0556
KIM BYEONG JU <100591433+hagisilm@users.noreply.github.com>
2024-03-25 15:41:07 +0900

    fix: 회원상태 삭제

--- src/main/java/com/nhnacademy/auth/user/repository/MemberStateRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nhnacademy.auth.user.repository;
-
-import com.nhnacademy.auth.user.entity.MemberState;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberStateRepository extends JpaRepository<MemberState,Long> {
-}
