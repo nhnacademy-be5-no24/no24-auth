af55215ee7d26a04fb0182198045230823b2169a
KIM BYEONG JU <100591433+hagisilm@users.noreply.github.com>
2024-04-03 23:55:56 +0900

    add: customer,grade,member restdocs 작성

--- src/main/java/com/nhnacademy/auth/user/controller/CustomerController.java
+++ src/main/java/com/nhnacademy/auth/user/controller/CustomerController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
  * 고객(Customer) RestController 입니다.
  *
  * @author : 김병주
- * @date : 2024-03-29
+ * @date : 2024-04-02
  */
 @RestController
 @RequiredArgsConstructor
@@ -21,18 +21,33 @@ import org.springframework.web.bind.annotation.*;
 public class CustomerController {
 
     private final CustomerService customerService;
-
+    /**
+     * 고객 단건 조회 요청 시 사용되는 메소드입니다.
+     *
+     * @param customerNo 조회를 위한 해당 고객 id 입니다.
+     * @return 성공했을 때 응답코드 200 OK 반환합니다.
+     */
     @GetMapping("/customer/{customerNo}")
     public ResponseEntity<CustomerDto> getCustomer(@PathVariable Long customerNo) {
         CustomerDto customerDto = customerService.getCustomer(customerNo);
         return ResponseEntity.ok(customerDto);
     }
-
+    /**
+     * 고객 생성 요청 시 사용되는 메소드입니다.
+     *
+     * @param customerCreateRequest 고객을 생성하기 위한 dto 입니다.
+     * @return 성공했을 때 응답코드 200 OK 반환합니다.
+     */
     @PostMapping("/customer/create")
     public ResponseEntity<CustomerDto> createCustomer(@RequestBody CustomerCreateRequest customerCreateRequest) {
         return ResponseEntity.status(HttpStatus.CREATED).body(customerService.createCustomer(customerCreateRequest));
     }
-
+    /**
+     * 고객 수정 요청 시 사용되는 메소드입니다.
+     * @param id 고객이 존재하는지 확인을 위한 id 입니다.
+     * @param customerCreateRequest 고객을 수정하기 위한 dto 입니다.
+     * @return 성공했을 때 응답코드 200 OK 반환합니다.
+     */
     @PutMapping("/customer/update/{id}")
     public ResponseEntity<CustomerDto> updateCustomer(@PathVariable Long id, @RequestBody CustomerCreateRequest customerCreateRequest) {
         return ResponseEntity.status(HttpStatus.CREATED).body(customerService.modifyCustomer(id, customerCreateRequest));
--- src/main/java/com/nhnacademy/auth/user/controller/MemberController.java
+++ src/main/java/com/nhnacademy/auth/user/controller/MemberController.java
@@ -8,30 +8,54 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+/**
+ * 회원(Member) RestController 입니다.
+ *
+ * @author : 김병주
+ * @date : 2024-04-02
+ */
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class MemberController {
     private final MemberService memberService;
-
-    @GetMapping("/member/{id}")
-    public ResponseEntity<MemberDto> getMember(@PathVariable Long id) {
-        return ResponseEntity.ok().body(memberService.getMember(id));
+    /**
+     * 회원 단건 조회 요청 시 사용되는 메소드입니다.
+     *
+     * @param memberId 조회를 위한 해당 회원 id 입니다.
+     * @return 성공했을 때 응답코드 200 OK 반환합니다.
+     */
+    @GetMapping("/member/{memberId}")
+    public ResponseEntity<MemberDto> getMember(@PathVariable Long memberId) {
+        return ResponseEntity.ok().body(memberService.getMember(memberId));
     }
-
+    /**
+     * 회원 생성 조회 요청 시 사용되는 메소드입니다.
+     *
+     * @param memberCreateRequest 회원을 생성하기 위한 dto 입니다.
+     * @return 성공했을 때 응답코드 201 CREATED 반환합니다.
+     */
     @PostMapping("/member/create")
     public ResponseEntity<MemberDto> createMember(@RequestBody MemberCreateRequest memberCreateRequest) {
         return ResponseEntity.status(HttpStatus.CREATED).body(memberService.createMember(memberCreateRequest));
     }
-
-    @PutMapping("/member/{id}")
-    public ResponseEntity<MemberDto> updateMember(@PathVariable Long id,@RequestBody MemberCreateRequest memberCreateRequest) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(memberService.modifyMember(id, memberCreateRequest));
+    /**
+     * 회원 수정 조회 요청 시 사용되는 메소드입니다.
+     * @param memberId 회원이 존재하는지 확인하기 위한 id 입니다.
+     * @param memberCreateRequest 회원을 수정하기 위한 dto 입니다.
+     * @return 성공했을 때 응답코드 201 CREATED 반환합니다.
+     */
+    @PutMapping("/member/{memberId}")
+    public ResponseEntity<MemberDto> updateMember(@PathVariable Long memberId,@RequestBody MemberCreateRequest memberCreateRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(memberService.modifyMember(memberId, memberCreateRequest));
     }
-
-    @DeleteMapping("/member/{id}")
-    public ResponseEntity<MemberDto> deleteMember(@PathVariable Long id) {
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(memberService.deleteMember(id));
+    /**
+     * 회원 삭제 요청 시 사용되는 메소드입니다.
+     * @param memberId 회원을 삭제하기 위한 id 입니다.
+     * @return 성공했을 때 응답코드 204 NO_CONTENT 반환합니다.
+     */
+    @DeleteMapping("/member/{memberId}")
+    public ResponseEntity<MemberDto> deleteMember(@PathVariable Long memberId) {
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(memberService.deleteMember(memberId));
     }
 }
